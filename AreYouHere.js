05c5a5ee7e65a9b515ec6f3adc5a8d4fe2bb6998
Julie <39500350+JGD625@users.noreply.github.com>
2019-08-09 16:32:24 -0400

    Create AreYouHere.js

---
+++ AreYouHere.js
@@ -0,0 +1,20 @@
+// What is the Big O of the following algorithm? Explain your answer
+
+// function areYouHere(arr1, arr2) {
+//     for (let i = 0; i < arr1.length; i++) {
+//         const el1 = arr1[i];
+//         for (let j = 0; j < arr2.length; j++) {
+//             const el2 = arr2[j];
+//             if (el1 === el2) return true;
+//         }
+//     }
+//     return false;
+// }
+
+
+// O(n^2) Polynomial time
+
+// This function is searching through two input arrays to find matching values
+// In the worst case, the function is looping through each element in the second
+// array for each element in the first array, so the time operations grow
+// bigger with input size
