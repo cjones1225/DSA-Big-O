23e253957511c6c087983ec74901913c6b0a520d
Julie <39500350+JGD625@users.noreply.github.com>
2019-08-09 16:33:04 -0400

    Create Doubler.js

---
+++ Doubler.js
@@ -0,0 +1,12 @@
+// What is the Big O of the following algorithm? Explain your answer
+
+ function doubleArrayValues(array) {
+     for (let i = 0; i < array.length; i++) {  // O(n)
+         array[i] *= 2;  // O(1)
+     }
+     return array;  // O(1)
+ }
+
+
+// O(n) Linear time
+// the most time consuming operation has a big O oproximation of Linear time
