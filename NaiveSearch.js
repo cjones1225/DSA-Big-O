f15149f381d3553676d0ac778e2e3bc302c14384
Julie <39500350+JGD625@users.noreply.github.com>
2019-08-09 16:33:48 -0400

    Create NaiveSearch.js

---
+++ NaiveSearch.js
@@ -0,0 +1,13 @@
+// What is the Big O of the following algorithm? Explain your answer
+
+ function naiveSearch(array, item) {
+     for (let i = 0; i < array.length; i++) {  // O(n)/        
+     if (array[i] === item) {  // O(1)
+             return i;  // O(1)
+         }
+     }
+ }
+
+
+// O(n) Linear time
+// the most time consuming operation has a big O oproximation of Linear time
