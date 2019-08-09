fb8bfe9636c84855da01d288b4b461cfcb3bc991
Julie <39500350+JGD625@users.noreply.github.com>
2019-08-09 16:25:52 -0400

    Create WhatAmI.js

---
+++ WhatAmI.js
@@ -0,0 +1,18 @@
+// What does the following algorithm do? What is the Big O 
+// of the following algorithm? Explain your answer
+
+ function isWhat(n) {
+     if (n < 2 || n % 1 != 0) {  // O(1)
+         return false;
+     }
+     for (let i = 2; i < n; ++i) {  // O(n)
+         if (n % i == 0) return false;
+     }
+     return true;  // O(1)
+ }
+
+// O(n) Linear time
+
+// The function checks to see if the input number is not a prime number.
+// The most time consuming operation loops a number of times equal to n-2,
+// checking to see if the input number is divisible by the current iteration.
